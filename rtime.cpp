bc66ee8edeec715a61000cc2b490b5268cd16c65
looting <695395@qq.com>
2021-01-19 10:08:35 +0800

    Add files via upload

---
+++ rtime.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+int main(int arg, char* argv[])
+{
+  if(arg<2) {
+    printf("Usage:\n    rtime<space>program_name\n\n");
+    return -1;
+  }
+  char szCMD[256] = {0};
+  sprintf(szCMD,"ps -eo pid,comm,etime | grep %s",(const char*)argv[1]);
+  system (szCMD);
+  return 0;
+}
