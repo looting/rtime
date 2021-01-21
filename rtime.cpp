9f420debb8a120903e4a3020ac06d8fe2721d128
looting <695395@qq.com>
2021-01-21 09:24:24 +0800

    Update rtime.cpp
    show title

--- rtime.cpp
+++ rtime.cpp
@@ -6,6 +6,7 @@ int main(int arg, char* argv[])
     printf("Usage:\n    rtime<space>program_name\n\n");
     return -1;
   }
+  printf(" PID\tNAME\t\tTIME\n");
   char szCMD[256] = {0};
   sprintf(szCMD,"ps -eo pid,comm,etime | grep %s",(const char*)argv[1]);
   system (szCMD);
